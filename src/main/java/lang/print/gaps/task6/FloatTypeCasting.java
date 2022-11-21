cb2d069a60cfdb4d62c58f7328bbfefc75fd59c0
Stas Sukora <st.sukora@gmail.com>
2022-11-22 00:47:39 +0300

    Update readme.md

--- src/main/java/lang/print/gaps/task6/FloatTypeCasting.java
+++ src/main/java/lang/print/gaps/task6/FloatTypeCasting.java
@@ -2,5 +2,9 @@ package lang.print.gaps.task6;
 
 public class FloatTypeCasting {
     public void roundNumber(float numberToBeRounded) {
+        int intVar = Math.round(numberToBeRounded);
+        System.out.println(intVar);
     }
+
 }
+
